a204b4de80ee0114810a81ba61edec98ef13f418
myoshikawa47 <m.yoshikawa47@gmail.com>
2025-07-15 14:55:29 +0900

    add docker environment for ros

---
+++ ros/docker/initialize-docker-container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# install octo
+source ~/.bashrc
+cd ~/lerobot && poetry install --extras "aloha xarm pusht"
+
+# git safe directory
+cd ~/lerobot && git config --global --add safe.directory /root/lerobot
+
+cd ~/lerobot
+
+echo "Finished setting up container"
+
+# https://stackoverflow.com/questions/30209776/docker-container-will-automatically-stop-after-docker-run-d
+tail -f /dev/null
+
