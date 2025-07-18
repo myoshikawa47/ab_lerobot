a99fc95d27b083ecc603359a5335b5a24579c8c0
myoshikawa47 <m.yoshikawa47@gmail.com>
2025-07-18 16:45:55 +0900

    fix vis_dataset.py and test.py

--- src/rosbag2lerobot/scripts/test.py
+++ src/rosbag2lerobot/scripts/test.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 import torch
 import argparse
 import numpy as np
@@ -15,8 +16,14 @@ from lerobot.datasets.lerobot_dataset import (
     LeRobotDatasetMetadata,
     MultiLeRobotDataset,
 )
+from lerobot.datasets.factory import resolve_delta_timestamps
 from lerobot.scripts.visualize_dataset import EpisodeSampler
+from lerobot.policies.smolvla.modeling_smolvla import SmolVLAPolicy
+from lerobot.policies.smolvla.configuration_smolvla import SmolVLAConfig
+from lerobot.policies.act.modeling_act import ACTPolicy
+from lerobot.policies.act.configuration_act import ACTConfig
 from lerobot.policies.diffusion.modeling_diffusion import DiffusionPolicy
+from lerobot.policies.diffusion.configuration_diffusion import DiffusionConfig
 from lerobot.policies.smolvla.modeling_smolvla import SmolVLAPolicy
 # from work.tmp.lerobot.policies.smolvla import smolvlm_with_expert
 
@@ -26,61 +33,66 @@ from lerobot.policies.smolvla.modeling_smolvla import SmolVLAPolicy
 # argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument("--ckpt_folder", type=str, default=None)
+parser.add_argument("--policy_type", choises=['smolvla', 'act', 'diffusion'], default='smolvla')
+parser.add_argument("--repo_id", type=str, default='null')
 parser.add_argument("--data_dir", type=str, default=None)
+parser.add_argument("--out_dir", type=str, default=None)
+parser.add_argument("--savename", type=str, default=None)
 parser.add_argument("--device", type=int, default=0)
 parser.add_argument("--idx", type=int, default=0)
 parser.add_argument("--webp", action="store_true")
 args = parser.parse_args()
 
-
-
-# Create a directory to store the video of the evaluation
-output_directory = Path("outputs/test/example_pusht_diffusion")
-output_directory.mkdir(parents=True, exist_ok=True)
-
-# Select your device
 device = 'cpu' if args.device < 0 else f"cuda:{args.device}"
 
-# Provide the [hugging face repo id](https://huggingface.co/lerobot/diffusion_pusht):
-# pretrained_policy_path = "lerobot/diffusion_pusht"
-# OR a path to a local outputs/train folder.
+# Load policy
 pretrained_policy_path = args.ckpt_folder
-
-policy = SmolVLAPolicy.from_pretrained(pretrained_policy_path)
+# # adapt device in config.json
+# config_path = os.path.join(pretrained_policy_path, "config.json")
+# with open(config_path, "r") as f:
+#     config = json.load(f)
+# config["device"] = device
+# with open(config_path, "w") as f:
+#     json.dump(config, f)
+if args.policy_type == 'smolvla':
+    policy = SmolVLAPolicy.from_pretrained(pretrained_policy_path)
+elif args.policy_type == 'act':
+    policy = ACTPolicy.from_pretrained(pretrained_policy_path)
+elif args.policy_type == 'diffusion':
+    policy = DiffusionPolicy.from_pretrained(pretrained_policy_path)
+policy.reset()
+policy.eval()
 policy.to(device)
 
-# We can verify that the shapes of the features expected by the policy match the ones from the observations
-# produced by the environment
 print(policy.config.input_features)
-
-# Similarly, we can check that the actions produced by the policy will match the actions expected by the
-# environment
 print(policy.config.output_features)
 
-# Reset the policy and environments to prepare for rollout
-policy.reset()
 
 # Load data
-# dataset = LeRobotDataset('null', root=args.data_dir)
-dataset = LeRobotDataset('lerobot/svla_so100_stacking')
+# dataset = LeRobotDataset('null', root=args.data_dir) # older version
+ds_meta = LeRobotDatasetMetadata(
+    args.repo_id, root=args.data_dir,
+)
+delta_timestamps = resolve_delta_timestamps(policy.config, ds_meta)
+dataset = LeRobotDataset(
+    args.repo_id,
+    root=args.data_dir,
+    delta_timestamps=delta_timestamps,
+)
 start = dataset.episode_data_index['from'][args.idx].item()
 end = dataset.episode_data_index['to'][args.idx].item()
 frames = [dataset[i] for i in range(start, end)]
 for k, v in frames[0].items():
-    if type(v)==torch.Tensor:
+    if isinstance(v, torch.Tensor):
         for frame in frames: 
             frame[k] = frame[k].unsqueeze(0).to(device)
 nloop = len(frames)
 
-def save_gt(frame):
-    return
 
 # Inference
 actions_list = []
-key_list = []
 for loop_ct in range(nloop):
     frame = frames[loop_ct]
-    save_gt(frame)
 
     # prediction
     action = policy.select_action(frame)
@@ -90,45 +102,57 @@ for loop_ct in range(nloop):
 
 actions_np = np.array(actions_list).reshape(nloop, -1)
 
+
 # plot images
 fig, ax = plt.subplots(1, 4, figsize=(24, 6), dpi=60)
 ax = ax.flatten()
 
+# ground truth
 gt = dict()
-gt = {k: np.array([frame[k].squeeze(0).cpu() for frame in frames]) for k, v in frames[0].items() if type(v)==torch.Tensor}
+gt = {k: np.stack([frame[k].squeeze(0).cpu().numpy() for frame in frames]) for k, v in frames[0].items() if type(v)==torch.Tensor}
+# prepare dataset which has multiple inputs or outputs for animation
+for k in gt.keys():
+    if ('image' in k and len(gt[k].shape) == 5) \
+        or ('observation.state' in k and len(gt[k].shape) == 3) \
+        or ('action' in k and len(gt[k].shape) == 3):
+            gt[k] = gt[k][:, 0]
+
 def anim_update(i):
-    for j in range(2):
+    print(i)
+    for j in range(len(ax)):
         ax[j].cla()
 
-    # plot camera image
-    ax[0].imshow(np.transpose(gt['observation.image.head.left'][i], (1, 2, 0)))
-    ax[0].axis("off")
-    ax[0].set_title("observation.image.head.left")
+    # # plot camera image
+    # ax[0].imshow(np.transpose(gt['observation.image.head.left'][i], (1, 2, 0)))
+    # ax[0].axis("off")
+    # ax[0].set_title("observation.image.head.left")
 
     # plot camera image
-    ax[1].imshow(np.transpose(gt['observation.image.arm.right'][i], (1, 2, 0)))
+    ax[1].imshow(np.transpose(gt['observation.image.head.right'][i, ::-1, ::10, ::10], (1, 2, 0)))
     ax[1].axis("off")
-    ax[1].set_title("observation.image.arm.right")
+    ax[1].set_title("observation.image.head.right")
 
     # plot joint angle
     ax[2].set_xlim(0, nloop)
-    ax[2].plot(gt['action'][:, :9], linestyle="dashed", c="k")
-    for joint_idx in range(9):
+    ax[2].plot(gt['action'], linestyle="dashed", c="k")
+    for joint_idx in range(gt['action'].shape[-1]):
         ax[2].plot(np.arange(i + 1), actions_np[: i + 1, joint_idx], c=f"C{joint_idx}")
     ax[2].set_xlabel("Step")
     ax[2].set_title("Joint angles")
     
-    # plot base angle
-    ax[3].set_xlim(0, nloop)
-    ax[3].plot(gt['action'][:, 9:], linestyle="dashed", c="k")
-    for joint_idx in range(9, 12):
-        ax[3].plot(np.arange(i + 1), actions_np[: i + 1, joint_idx], c=f"C{joint_idx}")
-    ax[3].set_xlabel("Step")
-    ax[3].set_title("Base angles")
+    # # plot base angle
+    # ax[3].set_xlim(0, nloop)
+    # ax[3].plot(gt['action'][:, 9:], linestyle="dashed", c="k")
+    # for joint_idx in range(9, 12):
+    #     ax[3].plot(np.arange(i + 1), actions_np[: i + 1, joint_idx], c=f"C{joint_idx}")
+    # ax[3].set_xlabel("Step")
+    # ax[3].set_title("Base angles")
 
 
 ani = anim.FuncAnimation(fig, anim_update, frames=nloop)
+os.path.mkdir(args.out_dir)
+savename = args.savename if args.savename else args.policy_type
 if args.webp:
-    ani.save(os.path.join(output_directory, "debug_{}.webp".format(args.idx)), writer="ffmpeg", fps=10)
+    ani.save(os.path.join(args.out_dir, "{}_{}.webp".format(savename, args.idx)), fps=10, writer="ffmpeg")
 else:
-    ani.save(os.path.join(output_directory, "debug_{}.mp4".format(args.idx)), fps=10, writer="ffmpeg")
+    ani.save(os.path.join(args.out_dir, "{}_{}.mp4".format(savename, args.idx)), fps=10, writer="ffmpeg")
--- src/rosbag2lerobot/scripts/vis_dataset.py
+++ src/rosbag2lerobot/scripts/vis_dataset.py
@@ -20,7 +20,6 @@ from lerobot.scripts.visualize_dataset import EpisodeSampler
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_dir", type=str, default=None)
 parser.add_argument("--out_dir", type=str, default=None)
-parser.add_argument("--device", type=int, default=0)
 parser.add_argument("--idx", type=int, default=0)
 parser.add_argument("--webp", action="store_true")
 args = parser.parse_args()
@@ -29,9 +28,6 @@ args = parser.parse_args()
 # Create a directory to store the video of the evaluation
 output_directory = Path(args.out_dir)
 output_directory.mkdir(parents=True, exist_ok=True)
-
-# Select your device
-device = 'cpu' if args.device < 0 else f"cuda:{args.device}"
 
 # Load data
 dataset = LeRobotDataset('null', root=args.data_dir)
@@ -46,10 +42,10 @@ ax = ax.flatten()
 
 gt = dict()
 gt = {k: np.array([frame[k].cpu() for frame in frames]) for k, v in frames[0].items() if type(v)==torch.Tensor}
-gt['observation.state.pos'] = np.concatenate([gt['observation.state.head'], gt['observation.state.left_arm.pos'], gt['observation.state.right_arm.pos']], axis=-1)
-gt['observation.state.torque'] = np.concatenate([gt['observation.state.left_arm.torque'], gt['observation.state.right_arm.torque']], axis=-1)
-gt['observation.state.tactile'] = np.concatenate([gt['observation.state.left_arm.tactile'], gt['observation.state.right_arm.tactile']], axis=-1)
-gt['action'] = np.concatenate([gt['action.head'], gt['action.left_arm'], gt['action.right_arm']], axis=-1)
+# gt['observation.state.pos'] = np.concatenate([gt['observation.state.head'], gt['observation.state.left_arm.pos'], gt['observation.state.right_arm.pos']], axis=-1)
+# gt['observation.state.torque'] = np.concatenate([gt['observation.state.left_arm.torque'], gt['observation.state.right_arm.torque']], axis=-1)
+# gt['observation.state.tactile'] = np.concatenate([gt['observation.state.left_arm.tactile'], gt['observation.state.right_arm.tactile']], axis=-1)
+# gt['action'] = np.concatenate([gt['action.head'], gt['action.left_arm'], gt['action.right_arm']], axis=-1)
 
 def anim_update(i):
     print(i)
@@ -78,33 +74,35 @@ def anim_update(i):
 
     # plot joint angle
     ax[4].set_xlim(0, nloop)
-    ax[4].plot(gt['observation.state.pos'], linestyle="dashed", c="k")
-    for joint_idx in range(9*2+2):
+    # ax[4].plot(gt['observation.state.pos'], linestyle="dashed", c="k")
+    ax[4].plot(gt['observation.state'], linestyle="dashed", c="k")
+    for joint_idx in range(2+9*2):
         ax[4].plot(np.arange(i + 1), gt['action'][: i + 1, joint_idx], c=f"C{joint_idx}")
     ax[4].set_xlabel("Step")
     ax[4].set_title("state-action")
     
-    # plot joint angle
-    ax[5].set_xlim(0, nloop)
-    ax[5].plot(gt['observation.state.torque'], linestyle="dashed", c="k")
-    for joint_idx in range(9*2):
-        ax[5].plot(np.arange(i + 1), gt['observation.state.torque'][: i + 1, joint_idx], c=f"C{joint_idx}")
-    ax[5].set_xlabel("Step")
-    ax[5].set_title("torque")
+    # # plot joint angle
+    # ax[5].set_xlim(0, nloop)
+    # ax[5].plot(gt['observation.state.torque'], linestyle="dashed", c="k")
+    # for joint_idx in range(9*2):
+    #     ax[5].plot(np.arange(i + 1), gt['observation.state.torque'][: i + 1, joint_idx], c=f"C{joint_idx}")
+    # ax[5].set_xlabel("Step")
+    # ax[5].set_title("torque")
    
-    # plot joint angle
-    ax[6].set_xlim(0, nloop)
-    ax[6].plot(gt['observation.state.tactile'], linestyle="dashed", c="k")
-    for joint_idx in range(2*2):
-        ax[6].plot(np.arange(i + 1), gt['observation.state.tactile'][: i + 1, joint_idx], c=f"C{joint_idx}")
-    ax[6].set_xlabel("Step")
-    ax[6].set_title("tactile")
+    # # plot joint angle
+    # ax[6].set_xlim(0, nloop)
+    # ax[6].plot(gt['observation.state.tactile'], linestyle="dashed", c="k")
+    # for joint_idx in range(2*2):
+    #     ax[6].plot(np.arange(i + 1), gt['observation.state.tactile'][: i + 1, joint_idx], c=f"C{joint_idx}")
+    # ax[6].set_xlabel("Step")
+    # ax[6].set_title("tactile")
 
     ax[7].axis("off")
 
 ani = anim.FuncAnimation(fig, anim_update, frames=nloop)
+savename = os.path.basename(args.data_dir)
 if args.webp:
-    ani.save(os.path.join(output_directory, "debug_{}.webp".format(args.idx)), writer="ffmpeg", fps=10)
+    ani.save(os.path.join(output_directory, "{}_{}.webp".format(savename, args.idx)), fps=10, writer="ffmpeg")
 else:
     print('writing mp4')
-    ani.save(os.path.join(output_directory, "debug_{}.mp4".format(args.idx)), fps=10, writer="ffmpeg")
+    ani.save(os.path.join(output_directory, "debug_{}.mp4".format(savename, args.idx)), fps=10, writer="ffmpeg")
